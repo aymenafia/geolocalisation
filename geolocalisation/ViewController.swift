9cbe1536b8dac7901f74ef7cb9eb2e679ffdb91f
Mohamed Aymen Afia <mohamedaymen.afia@esprit.tn>
2018-01-08 00:15:13 +0100

    get mario location

--- geolocalisation/ViewController.swift
+++ geolocalisation/ViewController.swift
@@ -8,19 +8,45 @@
 
 import UIKit
 import GoogleMaps
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
     var mapview : GMSMapView!
-    
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        
+        if CLLocationManager.locationServicesEnabled(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+        }
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
         let camera = GMSCameraPosition.camera(withLatitude: 43, longitude: -77, zoom: 10 )
-        
         mapview = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         self.view = mapview
         
     }
 
+    var oldLocation = CLLocationCoordinate2DMake(0, 0)
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        oldLocation = (manager.location?.coordinate)!
+        
+        let markerMe = GMSMarker()
+        markerMe.position = CLLocationCoordinate2DMake(oldLocation.latitude, oldLocation.longitude)
+        markerMe.title = "Me"
+        markerMe.snippet = "here is my location"
+        markerMe.icon = UIImage(named:"mario5")
+        markerMe.map = mapview
+        
+        
+        let camera = GMSCameraPosition.camera(withLatitude: oldLocation.latitude, longitude: oldLocation.longitude, zoom: 30 )
+        self.mapview.camera = camera
+        
+    }
 }
 
